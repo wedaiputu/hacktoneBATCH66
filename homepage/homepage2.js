cb48128900153ddccc728619736f1959a1dbd333
DerioAnjaya <anjayaderio@gmail.com>
2023-11-22 14:25:22 +0700

    homepage

---
+++ homepage/homepage2.js
@@ -0,0 +1,5 @@
+function startGame(gameName) {
+    // You can add logic here to start the selected game
+    alert("Starting " + gameName + "!");
+    // Redirect to the game page or load the game assets, etc.
+}
